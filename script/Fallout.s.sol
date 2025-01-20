e0e6af6f7162ee208bcf102077024ed5c997bac9
aestera <aest3ra@gmail.com>
2025-01-20 14:14:45 +0900

    [LEVEL 2] Fallout Solved

--- script/Fallout.s.sol
+++ script/Fallout.s.sol
@@ -8,14 +8,14 @@ import "forge-std/console.sol";
 contract FalloutSolution is Script {
 
     address payable target = 0x671268889DB29C3c66F0E112f9d271E643a26259;
-    Fallout fallout = Fallback(target);
+    Fallout fallout = Fallout(target);
 
     function run() external {
 
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
         fallout.Fal1out();
-        console.log("Owner: ", fallbackInstance.owner());
+        console.log("Owner: ", fallout.owner());
         console.log("My Address: ", vm.envAddress("MY_ADDRESS"));
 
         vm.stopBroadcast();
